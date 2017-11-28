d7ded12bf589a7ed468c67a79011123c732b684e
Filip Stangricki <stangricki@gmail.com>
2017-11-28 12:53:59 +0100

    First commit

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+$(function(){
+	//this code will execute after the DOM is loaded
+	var carouselList = $("#carousel ul");
+	setTimeout(changeSlide, 3000);
+	function changeSlide() {
+		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+	};
+	function moveFirstSlide() {
+
+		var lastItem = carouselList.find("li:last"),
+			firstItem = carouselList.find("li:first");
+
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft:0});
+	};
+
+});

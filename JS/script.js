3744d66926db8bd2b55cf0e66f8012f8bf417899
Krzysztof Safjanowski <krzysztof@safjanowski.pl>
2017-12-04 19:17:37 +0100

    Lets play with carousel

--- JS/script.js
+++ JS/script.js
@@ -1,47 +1,55 @@
+(function($) {
+  $.fn.carousel = function() {
+    return this.each(function(index, carousel) {
+      var carouselList = $("ul", carousel);
+      var interval = setInterval(changeSlide, 3000)
+
+      function changeSlide() {
+        carouselList.animate({
+          'marginLeft': -400
+        }, 500, moveSlide);
+      };
+
+      changeSlide.left = 'left'
+      changeSlide.right = 'right'
+
+      function moveSlide(direction) {
+        var lastItem, firstItem
+
+        if (direction === changeSlide.left) {
+          lastItem = carouselList.find("li:first");
+          firstItem = carouselList.find("li:last");
+
+          lastItem.before(firstItem);
+        } else {
+          lastItem = carouselList.find("li:last");
+          firstItem = carouselList.find("li:first");
+
+          lastItem.after(firstItem);
+        }
+
+        carouselList.css({
+          marginLeft: 0
+        });
+
+      };
+
+      $('.left', carousel).click(function() {
+        moveSlide(changeSlide.left)
+        clearInterval(interval)
+      })
+
+      $('.right', carousel).click(function() {
+        moveSlide(changeSlide.right)
+        clearInterval(interval)
+      })
+    })
+  }
+}(jQuery))
+
+
 $(function() {
 
-  var carouselList = $("#carousel ul");
-  var interval = setInterval(changeSlide, 3000)
-
-  function changeSlide() {
-    carouselList.animate({
-      'marginLeft': -400
-    }, 500, moveSlide);
-  };
-
-  changeSlide.left = 'left'
-  changeSlide.right = 'right'
-
-  function moveSlide(direction) {
-
-    var lastItem, firstItem
-
-    if (direction === changeSlide.left) {
-      lastItem = carouselList.find("li:first");
-      firstItem = carouselList.find("li:last");
-
-      lastItem.before(firstItem);
-    } else {
-      lastItem = carouselList.find("li:last");
-      firstItem = carouselList.find("li:first");
-
-      lastItem.after(firstItem);
-    }
-
-    carouselList.css({
-      marginLeft: 0
-    });
-
-  };
-
-  $('#left').click(function() {
-    moveSlide(changeSlide.left)
-    clearInterval(interval)
-  })
-
-  $('#right').click(function() {
-    moveSlide(changeSlide.right)
-    clearInterval(interval)
-  })
+  $(".carousel-wrapper").carousel()
 
 });
